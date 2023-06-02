9febeac7f64b675493b4c416804570384a663b5a
rishiranjan3003 <rishiranjan8924@gmail.com>
2023-06-02 17:29:07 +0530

    User Interactive App

---
+++ app/src/main/java/com/example/interactiveapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.interactiveapp
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("ResourceAsColor")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonDark = findViewById<Button>(R.id.btnDark)
+        val buttonRead = findViewById<Button>(R.id.btnRead)
+        val layout = findViewById<LinearLayout>(R.id.linearLayout)
+
+
+        buttonRead.setOnClickListener {
+            // change to light mode
+            layout.setBackgroundResource(R.color.yellow)
+        }
+
+        buttonDark.setOnClickListener {
+            layout.setBackgroundResource(R.color.black)
+        }
+    }
+}
